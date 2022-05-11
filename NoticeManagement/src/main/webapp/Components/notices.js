1bb376b95e5e8342e47f59b0d1b8a450848ffb46
Murfidh <IT20250492@my.sliit.lk>
2022-05-11 21:51:46 +0530

    proper code indentations

--- NoticeManagement/src/main/webapp/Components/notices.js
+++ NoticeManagement/src/main/webapp/Components/notices.js
@@ -17,119 +17,115 @@ if (status != true)
 // If valid------------------------
 var type = ($("#hidNoticeIDSave").val() == "") ? "POST" : "PUT";
  $.ajax(
- {
- url : "NoticeAPI",
- type : type,
- data : $("#formNotice").serialize(),
- dataType : "text",
- complete : function(response, status)
- {
- onNoticeSaveComplete(response.responseText, status);
- }
+	{
+		 url : "NoticeAPI",
+		 type : type,
+		 data : $("#formNotice").serialize(),
+ 		 dataType : "text",
+		 complete : function(response, status)
+ 	{
+ 	onNoticeSaveComplete(response.responseText, status);
+  }
  });
 });
 
-function onNoticeSaveComplete(response, status)
-{
-if (status == "success")
- {
- var resultSet = JSON.parse(response);
- if (resultSet.status.trim() == "success")
- {
- $("#alertSuccess").text("Successfully saved.");
- $("#alertSuccess").show();
- $("#divNoticesGrid").html(resultSet.data);
- } else if (resultSet.status.trim() == "error")
- {
- $("#alertError").text(resultSet.data);
- $("#alertError").show();
- }
- } else if (status == "error")
- {
- $("#alertError").text("Error while saving.");
- $("#alertError").show();
- } else
- {
- $("#alertError").text("Unknown error while saving..");
- $("#alertError").show();
- }  $("#hidNoticeIDSave").val("");
- $("#formNotice")[0].reset();
+function onNoticeSaveComplete(response, status){
+	
+		 if (status == "success"){
+		 var resultSet = JSON.parse(response);
+		 if (resultSet.status.trim() == "success"){
+		
+ 			$("#alertSuccess").text("Successfully saved.");
+ 			$("#alertSuccess").show();
+ 			$("#divNoticesGrid").html(resultSet.data);
+ 	}else if (resultSet.status.trim() == "error"){
+	
+			 $("#alertError").text(resultSet.data);
+ 			 $("#alertError").show();
+   }
+ }else if (status == "error"){
+	
+		 $("#alertError").text("Error while saving.");
+ 		 $("#alertError").show();
+ 	}else{
+	
+ 		 $("#alertError").text("Unknown error while saving..");
+ 		 $("#alertError").show();
+ 		 
+  }  	 
+ 		$("#hidNoticeIDSave").val("");
+ 		$("#formNotice")[0].reset();
 }
 
 
-function onNoticeDeleteComplete(response, status)
-{
-if (status == "success")
- {
- var resultSet = JSON.parse(response);
- if (resultSet.status.trim() == "success")
- {
- $("#alertSuccess").text("Successfully deleted.");
- $("#alertSuccess").show();
- $("#divNoticesGrid").html(resultSet.data);
- } else if (resultSet.status.trim() == "error")
- {
- $("#alertError").text(resultSet.data);
- $("#alertError").show();
- }
- } else if (status == "error")
- {
- $("#alertError").text("Error while deleting.");
- $("#alertError").show();
- } else
- {
- $("#alertError").text("Unknown error while deleting..");
- $("#alertError").show();
- }
+function onNoticeDeleteComplete(response, status){
+	
+if (status == "success"){
+	
+	 var resultSet = JSON.parse(response);
+	 if (resultSet.status.trim() == "success"){
+		
+ 		$("#alertSuccess").text("Successfully deleted.");
+ 		$("#alertSuccess").show();
+ 		$("#divNoticesGrid").html(resultSet.data);
+ 	}else if (resultSet.status.trim() == "error"){
+	
+ 		$("#alertError").text(resultSet.data);
+ 		$("#alertError").show();
+   }
+ }else if (status == "error"){
+	
+    	$("#alertError").text("Error while deleting.");
+ 		$("#alertError").show();
+ }else{
+ 		$("#alertError").text("Unknown error while deleting..");
+ 		$("#alertError").show();
+ 	}
 }
 
-$(document).on("click", ".btnRemove", function(event)
-{
- $.ajax(
- {
- url : "NoticeAPI",
- type : "DELETE",
- data : "ID=" + $(this).data("noticeid"),
- dataType : "text",
- complete : function(response, status)
- {
- onNoticeDeleteComplete(response.responseText, status);
- }
+$(document).on("click", ".btnRemove", function(event){
+	 $.ajax(
+		 {
+			 url : "NoticeAPI",
+ 			 type : "DELETE",
+   			 data : "ID=" + $(this).data("noticeid"),
+ 			 dataType : "text",
+ 			 complete : function(response, status)
+ 		{
+ 
+ 	onNoticeDeleteComplete(response.responseText, status);
+  }
  });
 });
 
 
-$(document).on("click", ".btnUpdate", function(event)
-{
-$("#hidNoticeIDSave").val($(this).data("itemid"));
- $("#title").val($(this).closest("tr").find('td:eq(0)').text());
- $("#description").val($(this).closest("tr").find('td:eq(1)').text());
- $("#branch").val($(this).closest("tr").find('td:eq(2)').text());
- $("#issuingOfficer").val($(this).closest("tr").find('td:eq(3)').text());
+$(document).on("click", ".btnUpdate", function(event){
+	
+		$("#hidNoticeIDSave").val($(this).data("noticeid"));
+ 		$("#title").val($(this).closest("tr").find('td:eq(0)').text());
+ 		$("#description").val($(this).closest("tr").find('td:eq(1)').text());
+ 		$("#branch").val($(this).closest("tr").find('td:eq(2)').text());
+ 		$("#issuingOfficer").val($(this).closest("tr").find('td:eq(3)').text());
 });
 
 
-function validateNoticeForm()
-{
-// CODE
-if ($("#title").val().trim() == "")
- {
- return "Insert Item Title.";
- }
-// NAME
-if ($("#description").val().trim() == "")
- {
- return "Insert Item Description.";
- } 
- // PRICE-------------------------------
-if ($("#branch").val().trim() == "")
- {
- return "Insert Item Branch.";
- }
+function validateNoticeForm(){
 
-if ($("#issuingOfficer").val().trim() == "")
- {
- return "Insert Item Issuing Officer.";
+	if ($("#title").val().trim() == ""){
+ 		return "Insert Item Title.";
+ 	}
+
+	if ($("#description").val().trim() == ""){
+		return "Insert Item Description.";
+ 	} 
+
+	if ($("#branch").val().trim() == ""){
+ 		return "Insert Item Branch.";
+ 	}
+
+	if ($("#issuingOfficer").val().trim() == ""){	
+	    return "Insert Item Issuing Officer.";
+ 	}
+	
+	return true;
  }
-return true;
-}
